4d19a0313bafa0893385a1b58e93515a45c39b6f
박정영 <bjy123bjy@gmail.com>
2024-10-28 17:43:58 +0900

    merge: 충돌 해결

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,8 +1,8 @@
 module.exports = {
   rules: {
     'type-enum': [
-      2, 
-      'always', 
+      2,
+      'always',
       [
         'feat',
         'fix',
@@ -20,11 +20,15 @@ module.exports = {
         'remove',
         'rename',
         'move',
-        'init'
+        'init',
+        'merge'
       ]
     ],
     'type-empty': [2, 'never'],
     'type-case': [2, 'always', 'lower-case'],
-    'subject-empty': [2, 'never']
+    'subject-empty': [2, 'never'],
+    'subject-full-stop': [0, 'never'],  // 마침표 규칙 비활성화
+    'header-max-length': [0, 'always', 72],  // 헤더 길이 제한 비활성화
+    'subject-case': [0, 'never']  // 대소문자 규칙 비활성화
   }
 };
