0518c555ecb6b3ebe4be6c257c59b774ddc1d1a4
hoiae <donald819@naver.com>
2024-11-06 10:15:41 +0900

    feat: setting swagger

--- backend/src/main/java/com/bwd4/mootd/controller/PhotoController.java
+++ backend/src/main/java/com/bwd4/mootd/controller/PhotoController.java
@@ -1,18 +1,17 @@
 package com.bwd4.mootd.controller;
 
 import com.bwd4.mootd.common.response.ApiResponse;
-import com.bwd4.mootd.dto.request.MapImagesRequestDTO;
 import com.bwd4.mootd.dto.request.PhotoUploadRequestDTO;
 import com.bwd4.mootd.dto.response.MapResponseDTO;
 import com.bwd4.mootd.service.PhotoService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
-import reactor.core.scheduler.Scheduler;
 
 import java.util.List;
 
@@ -29,6 +28,7 @@ public class PhotoController {
     }
 
     //TODO 촬영 기기의 고유정보를 입력받아야함.
+    @Operation(summary = "촬영 후 사진을 서버에 업로드하는 api", description = "촬영 사진을 서버로 업로드 합니다. !! 향후에 안드로이드 기기에서 가이드라인까지 생성한다면 가이드라인도 서버로 전송해야합니다.!!")
     @PostMapping(consumes = "multipart/form-data")
     public Mono<ResponseEntity<ApiResponse<String>>> uploadPhoto(PhotoUploadRequestDTO request) {
         //1.일단 "OK"d응답 성공을 반환한다.
@@ -39,12 +39,15 @@ public class PhotoController {
         return Mono.just(new ResponseEntity<>(ApiResponse.success("서버 전송이 완료되었습니다.", null), HttpStatus.OK));
     }
 
+    @Operation(summary = "위도, 경도, 반경(km)를 기반으로 해당 지역에 있는 사진목록을 조회합니다.")
     @GetMapping
     public Mono<ResponseEntity<ApiResponse<List<MapResponseDTO>>>> getImages(
-            @RequestParam double latitude,
-            @RequestParam double longitude,
-            @RequestParam double radius) {
-
+            @Parameter(description = "위도")
+            @RequestParam Double latitude,
+            @Parameter(description = "경도")
+            @RequestParam Double longitude,
+            @Parameter(description = "(위도,경도)기반으로 조회할 반경(km)")
+            @RequestParam Double radius) {
         return photoService.findPhotosWithinRadius(latitude, longitude, radius)
                 .collectList()  // 모든 데이터를 리스트로 변환
                 .map(list -> ResponseEntity.ok(ApiResponse.success("지도 이미지 조회 성공", list)));
--- backend/src/main/java/com/bwd4/mootd/dto/request/PhotoUploadRequestDTO.java
+++ backend/src/main/java/com/bwd4/mootd/dto/request/PhotoUploadRequestDTO.java
@@ -1,9 +1,12 @@
 package com.bwd4.mootd.dto.request;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import org.springframework.web.bind.annotation.RequestPart;
 import org.springframework.web.multipart.MultipartFile;
 
 public record PhotoUploadRequestDTO(
+        @Schema(description = "촬영한 원본 이미지")
         @RequestPart MultipartFile originImageFile,
-        @RequestPart String DeviceId){
+        @Schema(description = "기기의 식별자(고유아이디)")
+        @RequestPart String deviceId){
 }
