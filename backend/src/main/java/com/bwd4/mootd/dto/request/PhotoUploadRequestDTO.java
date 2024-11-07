465dabcbba7b6c1c5b55df515bda84cfe0cf18d3
hoiae <donald819@naver.com>
2024-11-07 10:19:44 +0900

    refactor: photo upload api
    위치정보를 파라미터로 받아서 저장하는 로직으ë로 수정

--- backend/src/main/java/com/bwd4/mootd/dto/request/PhotoUploadRequestDTO.java
+++ backend/src/main/java/com/bwd4/mootd/dto/request/PhotoUploadRequestDTO.java
@@ -5,8 +5,12 @@ import org.springframework.web.bind.annotation.RequestPart;
 import org.springframework.web.multipart.MultipartFile;
 
 public record PhotoUploadRequestDTO(
-        @Schema(description = "촬영한 원본 이미지")
-        @RequestPart MultipartFile originImageFile,
-        @Schema(description = "기기의 식별자(고유아이디)")
-        @RequestPart String deviceId){
+        @Schema(description = "촬영한 원본 이미지", required = true)
+        MultipartFile originImageFile,
+        @Schema(description = "기기의 식별자(고유아이디)", required = true)
+        String deviceId,
+        @Schema(description = "위도", required = true)
+        Double latitude,
+        @Schema(description = "경도", required = true)
+        Double longitude){
 }
