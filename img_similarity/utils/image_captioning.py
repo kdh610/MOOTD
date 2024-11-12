291b90c5290c849a7e445107158e4c37f5a62049
박경령 <fbxm12@naver.com>
2024-11-12 20:56:22 +0900

    feat: replace captioning model

--- img_similarity/utils/image_captioning.py
+++ img_similarity/utils/image_captioning.py
@@ -1,4 +1,5 @@
-from gpt2_image_captioning.captioning_utils import make_caption_list
+# from gpt2_image_captioning.captioning_utils import make_caption_list
+from gpt2_image_captioning.cap_pipline import make_caption_list
 
 def generate_caption_keywords(image_path):
     # 이미지에서 단어 리스트 생성
