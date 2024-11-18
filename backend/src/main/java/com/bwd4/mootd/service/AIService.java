930576b88f7ca2dcacd5cf82c6f01d9e152cd9ef
hoiae <donald819@naver.com>
2024-11-18 18:26:47 +0900

    chore: rollback

--- backend/src/main/java/com/bwd4/mootd/service/AIService.java
+++ backend/src/main/java/com/bwd4/mootd/service/AIService.java
@@ -48,7 +48,6 @@ public class AIService {
                 .build();
 
         this.guideLineAiClient = WebClient.builder()
-//                .baseUrl("http://" + "70.12.130.101:61423/")
                 .baseUrl("http://" + HOST + ":8002")
                 .clientConnector(new ReactorClientHttpConnector(
                         HttpClient.create().responseTimeout(java.time.Duration.ofMinutes(2))
